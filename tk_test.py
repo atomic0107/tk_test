14eca02e45b5f9c8383d9b93c2e783bf508bc543
tomohiro udagawa <eggo.world@gmail.com>
2019-02-03 22:29:51 +0900

    add
    add

--- tk_test.py
+++ tk_test.py
@@ -27,11 +27,11 @@ prime_cnt = 50
 #dict = json.load(json_t)
 
 dict = {
-    "mind":None,
-    "net":None,
-    "IT":None,
-    "swk":None,
-    "tom":None
+    u"mind":None,
+    u"net":None,
+    u"IT":None,
+    u"swk":None,
+    u"tom":None
 }
 
 class mind():
@@ -42,42 +42,106 @@ class mind():
         self.textobj_list = []
         self.textbox_list = []
         self.textbox_cnt = 0
-        #self.panel = panel
-        #self.panel.Bind(wx.EVT_LEFT_DOWN, self.click_panel)
-        root = tkinter.Tk()
+        self.labellist=[]
 
-        frame = tkinter.Frame(root, width=bd_width, height=50)
-        frame.bind("<Tab>", self.callback)#マウスポインタ-がwidgetに入ったとき
-        frame.pack()
+        self.dict_list = list(dict.keys())
+        dict_len = len(self.dict_list)
+        for text_cnt in range( dict_len ):
+            self.y = center_y + text_cnt * 25
+            self.Static = tkinter.Label( text = self.dict_list[text_cnt] )
 
+            self.Static.bind('<Button-1>', self.click_label)#click
+            self.Static.bind('<Button1-Motion>', self.label_drag)
+            self.Static.bind('<Double-Button-1>', self.edit_label)#double click
 
-        self.c0 = tkinter.Canvas(root, width=bd_width, height=bd_height)
-        self.c0.pack()
-        self.id1 = self.c0.create_rectangle(10, 10, 20, 20, fill = 'red')
-        self.id3 = self.c0.create_rectangle(30, 30, 50, 50, fill = 'blue')
+            self.labellist.append( self.Static )
+            self.Static.place( x = center_x ,y = self.y )
+            print(self.labellist[text_cnt].cget("text"))#MAP要素をプリント
         # バインディング
-        self.c0.tag_bind(self.id1, '<Button1-Motion>', self.rect_drag)
-        self.c0.tag_bind(self.id3, '<Double-Button-1>', self.move_rect)
 
-        root.mainloop()
+        #self.Static.bind('<Tab>', self.create_label)
+    def click_label(self,event):
 
-        # 移動-
-    def rect_drag(self,event):
+        #print(self.Static.cget("text"))
+        print(event.widget["text"])
+
+        print(event.widget.winfo_x())
+        print(event.widget.winfo_y())
+        #print(event.widget["y"])
+
+    def label_drag(self,event):
         #c0 = event.widget
-        x = event.x
-        y = event.y
-        self.c0.coords(self.id1, x - 5, y - 5, x + 5, y + 5)
+        print(self.Static.text)
+        self.Static.place(x=event.x ,y=event.y)
 
-    def move_rect(self,event):
+    def edit_label(self,event):
         #c0 = event.widget
-        print("print move_rect")
-        self.id1 = self.c0.create_rectangle(10, 10, 20, 20, fill = 'red')
+        #print("print move_rect")
+        print("#### edit label ####")
+        ##self.Static = tkinter.Label(text=u'test1')
+        #self.Static.place(x=center_x/2 ,y=50)
+        #self.labellist.append(self.Static2)
+        #self.Static.bind('<Button1-Motion>', self.label_drag)
+        #self.Static.bind('<Double-Button-1>', self.create_label)
+        #print(self.labellist[0].cget("text"))
+        #print(self.labellist[1].cget("text"))
 
     def callback(self,event):
         print("callback")
 
+class Main():
+    def __init__(self,root):
+        """ レイアウトの作成 """
+        #wx.Frame.__init__(self, parent, id, title)
+        #frame = wx.Frame(None,title="mindnet")#ウィンドウ作成クラス
+        #frame.SetClientSize(bd_width,bd_height)
+        #panel = wx.Panel(frame)
+        #panel.SetBackgroundColour(clr)#set color of background
+
+        self.canv = tkinter.Canvas(root, width=bd_width, height=bd_height)
+        self.canv.pack()
+        self.id1 = self.canv.create_rectangle(10, 10, 20, 20, fill = 'red')
+        self.id3 = self.canv.create_rectangle(30, 30, 50, 50, fill = 'blue')
+        self.canv.tag_bind(self.id1, '<Button1-Motion>', self.rect_drag)
+        self.canv.tag_bind(self.id1, '<Tab>', self.rect_create,"+")
+        self.canv.tag_bind(self.id3, '<Double-Button-1>', self.rect_create_red)
+        self.canv.tag_bind(self.id3, '<Button1-Motion>', self.rect_drag3)
+        self.canv.bind('<Tab>', self.tab_ev)
+        mind()
+
+    def tab_ev(self,event):
+        self.Static = tkinter.Label(text=u'canv_tab')
+        #self.labellist.append(self.Static)
+        self.Static.place(x=center_x/2 ,y=100)
+
+    def rect_drag(self,event):
+        #c0 = event.widget
+        x = event.x
+        y = event.y
+        self.canv.coords(self.id1, x - 5, y - 5, x + 5, y + 5)
+
+    def rect_drag3(self,event):
+        #c0 = event.widget
+        x = event.x
+        y = event.y
+        self.canv.coords(self.id3, x - 5, y - 5, x + 5, y + 5)
+
+    def rect_create(self,event):
+        #c0 = event.widget
+        print("print move_rect")
+        self.id1 = self.canv.create_rectangle(10, 10, 20, 20, fill = 'red')
+
+    def rect_create_red(self,event):
+        #c0 = event.widget
+        print("print move_rect")
+        self.id1 = self.canv.create_rectangle(10, 10, 20, 20, fill = 'red')
+
 def main():
-    mind()
+    root = tkinter.Tk()
+    root.title(u"Software Title")
+    root.geometry("500x500")
+    Main(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
