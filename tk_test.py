15ccd381fce3b5b68334518df9e90690c1783f77
tomohiro <eggo.world@gmail.com>
2019-02-07 01:03:26 +0900

    add
    add bug fix

--- tk_test.py
+++ tk_test.py
@@ -1,8 +1,8 @@
+# coding: UTF-8
 '''
 Created on 2019/01/29
 
 @author: tom_uda
-
 
 http://d.hatena.ne.jp/Cassiopeia/20070821/1187701922
 '''
@@ -35,44 +35,39 @@ dict = {
 }
 
 class mind():
-    #コンストラクタ
+    #class variable
+    cur_len = 0
+    cur_x = center_x
+    cur_y = center_y
+    tab_flag = 0
+    #constractor
     def __init__(self):
-        self.x = center_x
-        self.y = center_y
         self.textobj_list = []
         self.textbox_list = []
         self.textbox_cnt = 0
         self.labellist=[]
-
+        self.x = mind.cur_x
+        self.y = mind.cur_y
         self.dict_list = list(dict.keys())
-        dict_len = len(self.dict_list)
-        for text_cnt in range( dict_len ):
+        mind.cur_len = len(self.dict_list)
+        #dict_len = len(self.dict_list)
+        for text_cnt in range( mind.cur_len ):
             self.y = center_y + text_cnt * 25
+            self.x = center_x
             self.Static = tkinter.Label( text = self.dict_list[text_cnt] )
-
             self.Static.bind('<Button-1>', self.click_label)#click
-            self.Static.bind('<Button1-Motion>', self.label_drag)
+            #self.Static.bind('<Button1-Motion>', self.label_drag)
             self.Static.bind('<Double-Button-1>', self.edit_label)#double click
-
             self.labellist.append( self.Static )
-            self.Static.place( x = center_x ,y = self.y )
+            self.Static.place( x = self.x ,y = self.y )
             print(self.labellist[text_cnt].cget("text"))#MAP要素をプリント
-        # バインディング
+        #mind.cur_x = self.x
+        #mind.cur_y = self.y
 
-        #self.Static.bind('<Tab>', self.create_label)
     def click_label(self,event):
-
-        #print(self.Static.cget("text"))
         print(event.widget["text"])
-
         print(event.widget.winfo_x())
         print(event.widget.winfo_y())
-        #print(event.widget["y"])
-
-    def label_drag(self,event):
-        #c0 = event.widget
-        print(self.Static.text)
-        self.Static.place(x=event.x ,y=event.y)
 
     def edit_label(self,event):
         #c0 = event.widget
@@ -86,7 +81,23 @@ class mind():
         editbox.place( x = edit_x,y = edit_y )
         editbox.focus_set()#指定ウィジェットをアクティブにする
         editbox.bind( '<Return>', self.update_label )#enter key
-        editbox.bind( '<Escape>', self.delete )#enter key
+        editbox.bind( '<Escape>', self.cls_delete )#esc key
+        editbox.bind( '<Leave>', self.cls_leave )#widget leave
+
+    @classmethod
+    def entry_label(cls):
+        editbox = tkinter.Entry()
+        cls.cur_y = center_y + cls.cur_len * 25
+        cls.tab_flag = 0
+        editbox.place( x = cls.cur_x, y = cls.cur_y )
+        editbox.focus_set()#指定ウィジェットをアクティブにする
+        editbox.bind( '<Return>', cls.write_label )#enter key
+        editbox.bind( '<Escape>', cls.cls_delete )#esc key
+        editbox.bind( '<Leave>', cls.cls_delete )#leave pointer
+        editbox.bind( '<Tab>', cls.cls_tab )#tab key
+        #cls.tab_flag += 1
+        text = editbox.get()
+        print(text)
 
     def update_label(self,event):
         text = event.widget.get()
@@ -94,37 +105,66 @@ class mind():
         self.temp_label["text"] = text
         print(text)
 
-    def delete(self,event):
+    @classmethod
+    def cls_tab(clt,event):
+        print("mind tab")
+        if clt.tab_flag > 0 :
+            event.widget.destroy()
+        clt.tab_flag = 1
+
+    @classmethod
+    def write_label(clt,event):
+        text = event.widget.get()
+        label_x = event.widget.winfo_x()
+        label_y = event.widget.winfo_y()
         event.widget.destroy()
+        Static = tkinter.Label(text=text)
+        Static.place( x = label_x ,y = label_y )
+        mind.cur_len += 1
+
+    @classmethod
+    def cls_leave(cls,event):
+        event.widget.destroy()
+        mind.cur_len -= 1
+
+    @classmethod
+    def cls_delete(cls,event):
+        event.widget.destroy()
+        mind.cur_len -= 1
+
     def nop(self,event):
         pass
+
     def callback(self,event):
         print("callback")
 
 class Main():
+
     def __init__(self,root):
         """ レイアウトの作成 """
-        #wx.Frame.__init__(self, parent, id, title)
-        #frame = wx.Frame(None,title="mindnet")#ウィンドウ作成クラス
-        #frame.SetClientSize(bd_width,bd_height)
-        #panel = wx.Panel(frame)
-        #panel.SetBackgroundColour(clr)#set color of background
-
+        #global tab_flag
+        self.tab_flag = False
         self.canv = tkinter.Canvas(root, width=bd_width, height=bd_height)
         self.canv.pack()
         self.id1 = self.canv.create_rectangle(10, 10, 20, 20, fill = 'red')
         self.id3 = self.canv.create_rectangle(30, 30, 50, 50, fill = 'blue')
         self.canv.tag_bind(self.id1, '<Button1-Motion>', self.rect_drag)
-        self.canv.tag_bind(self.id1, '<Tab>', self.rect_create,"+")
+        #self.canv.tag_bind(self.id1, '<Tab>', self.rect_create,"+")
         self.canv.tag_bind(self.id3, '<Double-Button-1>', self.rect_create_red)
         self.canv.tag_bind(self.id3, '<Button1-Motion>', self.rect_drag3)
-        self.canv.bind('<Tab>', self.tab_ev)
+        #self.canv.bind('<Tab>', self.tab_ev)
+        root.bind('<Tab>', self.tab_ev)
         mind()
 
     def tab_ev(self,event):
-        self.Static = tkinter.Label(text=u'canv_tab')
+        print("main tab")
+        if self.tab_flag == False:
+            mind.entry_label()
+            self.tab_flag = True
+
+        #self.Static = tkinter.Label(text=u'canv_tab')
         #self.labellist.append(self.Static)
-        self.Static.place(x=center_x/2 ,y=100)
+        #self.Static.place(x=center_x/2 ,y=100)
 
     def rect_drag(self,event):
         #c0 = event.widget
