7c96e4420a823e407bc8eead45d244d70867466c
tomohiro udagawa <eggo.world@gmail.com>
2019-02-10 13:14:00 +0900

    add fix desc
    add fix desc

--- tk_test.py
+++ tk_test.py
@@ -11,13 +11,12 @@ import json
 ##############################################
 #            global variable
 ##############################################
-rec_side = 50       #四角の辺の大きさ
-bd_width = 500     #フレームの横幅
-bd_height = 500    #フレームの縦幅
+bd_width = 500     #frame h
+bd_height = 500    #frame w
 center_x = bd_width/2
 center_y = bd_height/2
-clr = "black"       #背景の色
-tclr = 'green'       #テキストの色
+clr = "black"#background color
+tclr = 'green'#text color
 prime_cnt = 50
 #f = open('data.json', 'r')
 #f = open('data_w.json', 'w')
@@ -60,7 +59,7 @@ class mind():
             self.Static.bind('<Double-Button-1>', self.edit_label)#double click
             self.labellist.append( self.Static )
             self.Static.place( x = self.x ,y = self.y )
-            print(self.labellist[text_cnt].cget("text"))#MAP要素をプリント
+            print(self.labellist[text_cnt].cget("text"))#print mapinfo
         #mind.cur_x = self.x
         #mind.cur_y = self.y
 
@@ -79,7 +78,7 @@ class mind():
         editbox = tkinter.Entry()
         editbox.insert(tkinter.END,self.temp_label["text"])
         editbox.place( x = edit_x,y = edit_y )
-        editbox.focus_set()#指定ウィジェットをアクティブにする
+        editbox.focus_set()#editbox active
         editbox.bind( '<Return>', self.update_label )#enter key
         editbox.bind( '<Escape>', self.cls_delete )#esc key
         editbox.bind( '<Leave>', self.cls_leave )#widget leave
@@ -90,7 +89,7 @@ class mind():
         cls.cur_y = center_y + cls.cur_len * 25
         cls.tab_flag = 0
         editbox.place( x = cls.cur_x, y = cls.cur_y )
-        editbox.focus_set()#指定ウィジェットをアクティブにする
+        editbox.focus_set()#editbox active
         editbox.bind( '<Return>', cls.write_label )#enter key
         editbox.bind( '<Escape>', cls.cls_delete )#esc key
         editbox.bind( '<Leave>', cls.cls_delete )#leave pointer
@@ -98,13 +97,13 @@ class mind():
         #cls.tab_flag += 1
         text = editbox.get()
         print(text)
-        
+
     @classmethod
     def cls_tab(cls,event):
         print("mind tab")
-        if cls.tab_flag == False:#tab unpressed case 
+        if cls.tab_flag == False:#tab unpressed case
             event.widget.destroy()
-        cls.tab_flag = 1
+        cls.tab_flag = True#set tab press
 
     @classmethod
     def write_label(cls,event):
@@ -115,17 +114,19 @@ class mind():
         Static = tkinter.Label(text=text)
         Static.place( x = label_x ,y = label_y )
         mind.cur_len += 1
-        cls.tab_flag = False#tab unpressed
+        cls.tab_flag = False#set tab unpressed
 
     @classmethod
     def cls_leave(cls,event):
         event.widget.destroy()
-        mind.cur_len -= 1
+        #mind.cur_len -= 1
+        cls.tab_flag = False#set tab unpressed
 
     @classmethod
     def cls_delete(cls,event):
         event.widget.destroy()
-        mind.cur_len -= 1
+        #mind.cur_len -= 1
+        cls.tab_flag = False#set tab unpressed
 
     def update_label(self,event):
         text = event.widget.get()
